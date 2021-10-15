52a503713dc041ef43edadb53dbad082b5a51e8a
Rafael Porto <rafaelgportof@gmail.com>
2021-10-15 07:50:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ function lista(){
     var produtos = document.getElementById('produto');
     var preco = document.getElementById('preco');
     var register = document.getElementById('cadasSingle');
+    itens.push({
+        nome: produtos.value,
+        valor: preco.value
+    });
+    if(produtos.value.length == 0 || preco.value.length == 0){
+        alert('[ERROR]')
+    }else{
+        var listaProdutos = document.getElementById('cadasSingle');
 }
